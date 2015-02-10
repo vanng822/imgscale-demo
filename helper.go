ff7a538e0469b7eb5cf7e65173544e8e477099af
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-02-10 09:46:21 +0100

    Make link to github

--- helper.go
+++ helper.go
@@ -30,10 +30,10 @@ func indexHandler(res http.ResponseWriter, req *http.Request) string {
 				<body>
 					<div style="text-align: center;">
 					<p>
-						Demo server for https://github.com/vanng822/imgscale
+						Demo server for <a href="https://github.com/vanng822/imgscale" target="_blank">https://github.com/vanng822/imgscale</a>
 					</p>
 					<p>
-						<a href="/img/0x360/http://images4.fanpop.com/image/photos/16100000/Cute-Kitten-kittens-16123796-1280-800.jpg">
+						<a href="/img/0x360/http://images4.fanpop.com/image/photos/16100000/Cute-Kitten-kittens-16123796-1280-800.jpg" target="_blank">
 							<img src="/img/100x0/http://images4.fanpop.com/image/photos/16100000/Cute-Kitten-kittens-16123796-1280-800.jpg" />
 						</a>
 					</p>
